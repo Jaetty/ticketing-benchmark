05021c5287a2aebddee4405c63e269d2783f315a
sangjae <dlbia2009@gmail.com>
2025-06-08 17:33:28 +0900

    feature : Swagger 추가

--- monolith-backend/src/main/java/io/github/jaetty/benchmark/user/controller/UserController.java
+++ monolith-backend/src/main/java/io/github/jaetty/benchmark/user/controller/UserController.java
@@ -3,19 +3,17 @@ package io.github.jaetty.benchmark.user.controller;
 import io.github.jaetty.benchmark.global.response.CustomBody;
 import io.github.jaetty.benchmark.global.response.StatusEnum;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
     @GetMapping("/search") // 아이디를 기준으로 확인 체크
     public ResponseEntity<CustomBody> getUsers(@RequestParam String loginId) {
-
-        /* github action 변경 확인 테스트 4 */
 
         return ResponseEntity.ok(new CustomBody(StatusEnum.OK, "정상", "테스트 중"));
     }
