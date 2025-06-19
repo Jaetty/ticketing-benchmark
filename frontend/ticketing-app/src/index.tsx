3dcda6904e8f5b5e698377b50cb5a56923c1c250
sangjae <dlbia2009@gmail.com>
2025-06-19 09:12:51 +0900

    Feature : zustand 상태 관리로 트래픽 설정 코드 수정

--- frontend/ticketing-app/src/index.tsx
+++ frontend/ticketing-app/src/index.tsx
@@ -5,13 +5,17 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css'; // tailwind 적용
 
 import App from "./App";
+import Header from "./components/Common/Header/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Header/>
+      <div className="pt-6"> {/* 64px == 16 * 4px */}
+        <App />
+      </div>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ frontend/ticketing-app/src/stores/index.tsx
@@ -0,0 +1,1 @@
+export {trafficSettingStore} from './trafficSettingStore'
