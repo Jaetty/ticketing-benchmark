738527b2a56e93069535daa7172cba886c88788e
sangjae <dlbia2009@gmail.com>
2025-07-01 13:41:56 +0900

    Refactor: 헤더 멈추기 버튼 주석 처리

--- frontend/ticketing-app/src/components/Common/Header/Header.tsx
+++ frontend/ticketing-app/src/components/Common/Header/Header.tsx
@@ -1,19 +1,19 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import { trafficSettingStore } from 'stores';
 
 export default function Header() {
 
     const started = trafficSettingStore((state) => state.started);
-    const setStarted = trafficSettingStore((state) => state.setStarted);
-    const navigate = useNavigate();
+    // const setStarted = trafficSettingStore((state) => state.setStarted);
+    // const navigate = useNavigate();
 
-    const handleStopClick = () => {
-        // 여기에 추후 백엔드 API 호출과 리다이렉션을 넣기
-        alert("테스트를 멈춥니다!");
-        setStarted(false);
-        navigate("/result");
-    }
+    // const handleStopClick = () => {
+    //     // 여기에 추후 백엔드 API 호출과 리다이렉션을 넣기
+    //     alert("테스트를 멈춥니다!");
+    //     setStarted(false);
+    //     navigate("/result");
+    // }
 
     return (
     <header className="w-full bg-white shadow-md fixed top-0 left-0 z-50">
@@ -28,11 +28,11 @@ export default function Header() {
             </div>
 
         {/* 로그인/회원가입 or 프로필 */}
-        {started && (
+        {/* {started && (
             <div className="space-x-4 hidden md:flex">
           <button onClick={handleStopClick} className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700 text-sm">테스트 멈추기</button>
         </div>
-        )}
+        )} */}
       </div>
     </header>
   );
